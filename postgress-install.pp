06c512062014be8635a8f2d5f8a7509e71866c83
M_chowdhury <59116892+SgtClutch@users.noreply.github.com>
2024-02-05 08:48:16 -0500

    Update postgress-install.pp

--- postgress-install.pp
+++ postgress-install.pp
@@ -30,29 +30,23 @@ class profile::postgresql  {
 
   file_line { 'allow_all_users_ipv4':
     path    => '/etc/postgresql/14/main/pg_hba.conf',
-    line    => 'host    all             all             0.0.0.0/0            md5',
-    match   => '^host\s+all\s+all\s+0\.0\.0\.0/0\s+md5$',
-    ensure  => present,
-    require => Service['postgresql'],
+    line    => 'host    all             all             0.0.0.0/0               md5',
+    match   => '^host\s+all\s+all\s+127.0.0.1/32\s+\sscram-sha-256$',
+    notify  => Service['postgresql'],
+    
 
   }
 
   file_line { 'all_listen_address':
-  path    => '/etc/postgresql/14/main/postgresql.conf,
-  line    => 'listen_address=*',
-  match   => '^#listen_address = ',
-  after   => '^#.*$', # This makes sure the line is added after any existing commented lines
+    path    => '/etc/postgresql/14/main/postgresql.conf',
+    line    => "listen_addresses = '*'",
+    match   => "^#listen_addresses = 'localhost'",
+    notify  => Service['postgresql'],
+    
 
   }
 
 
 
-  exec { 'set_postgres_password':
-    command => 'sudo -u postgres psql -c "ALTER USER postgres PASSWORD \'TPSrep0rt!\';"',
-    unless  => 'sudo -u postgres psql -c "SELECT 1" | grep -q 1',
-    require => Service['postgresql'],
-    refreshonly => true,
-    path => '/usr/bin/sudo',
-  }
 
  }
